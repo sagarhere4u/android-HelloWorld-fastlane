af32ab56d8651e172d3bd5996400267c3401d5ee
Sagar Mehta <sagar.mehta@atgensoft.com>
2020-12-23 08:50:05 -0800

    new automate

---
+++ fastbeta.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+/opt/android-sdk/tools/bin/sdkmanager "platforms;android-23" "build-tools;23.0.2" "extras;google;m2repository" "extras;android;m2repository"
+cd /project
+fastlane android beta
